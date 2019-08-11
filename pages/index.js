f74fdf2ac7cf18a42139fe1880efbb644c7dd026
aquasar <arhoy@ualberta.ca>
2019-08-11 16:26:05 -0600

    make better

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ const Index = () => (
     <Layout title =  "Home">
         <div>
             <h1>Welcome to the Home page</h1>
-            <p>Fuck you bitch</p>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
     </Layout>
 )
